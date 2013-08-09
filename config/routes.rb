a9520ef715d1c00c50fbafd9c174d4dce3b6eb11
Michael Crist <jmcrist@gmail.com>
2013-08-08 22:06:20 -0500

    Change gem file to install sqlite 1.3.7

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ IdeaFarm::Application.routes.draw do
     resources :comments
   end
   resources :sessions, only: [:new, :create, :destroy]
-  root  'static_pages#home'
+  root  'ideas#index'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
